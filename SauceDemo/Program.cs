7cfbd17cc7462d509437f209cb5292e7e4ae9b42
themmixproject <steven.devalk@gmail.com>
2023-10-21 16:18:17 +0200

    Added driver initialisation and startup, username is being used in path for edge executable

--- SauceDemo/Program.cs
+++ SauceDemo/Program.cs
@@ -1,9 +1,31 @@
-﻿using System;
+﻿using OpenQA.Selenium.Edge;
+using OpenQA.Selenium;
+using System;
 using System.Configuration;
 
 internal class Program {
+    public static IWebDriver driver;
     private static void Main( string[] args ) {
+        string binaryPath = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe";
+        string driverPath = "C:\\Webdrivers\\msedgedriver.exe";
+
         string username = getUsernameFromSecretsConfig();
+        string profilePath = "C:\\Users\\" + username +"\\AppData\\Local\\Microsoft\\Edge\\Test User Data";
+
+        EdgeOptions options = new EdgeOptions {
+            BinaryLocation = binaryPath
+        };
+        options.AddArgument( "--no-sandbox" ); // If running in a restricted environment
+        options.AddArgument( "--disable-dev-shm-usage" ); // If running in a restricted environment
+        options.AddArgument( "--disable-gpu" ); // If you encounter GPU-related issues
+        options.AddArgument( "--disable-features=msUndersideButton" );
+        options.AddArgument( "--disable-features=msShowSignInIndicator" );
+        options.AddArgument( "--log-level=3" );
+        options.AddArgument( "user-data-dir=" + profilePath );
+
+        driver = new EdgeDriver( driverPath, options );
+
+        driver.Quit();
 
         Console.WriteLine( "Hello, World!" );
     }
