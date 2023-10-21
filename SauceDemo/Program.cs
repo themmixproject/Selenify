39598d587ad72dd262c1bbe76673885cea4da529
themmixproject <steven.devalk@gmail.com>
2023-10-21 16:07:14 +0200

    Added getUsernamefromSecretsConfig method, added setting to always copy secrets.config file when the project builds

--- SauceDemo/Program.cs
+++ SauceDemo/Program.cs
@@ -1,5 +1,24 @@
-﻿internal class Program {
+﻿using System;
+using System.Configuration;
+
+internal class Program {
     private static void Main( string[] args ) {
+        string username = getUsernameFromSecretsConfig();
+
         Console.WriteLine( "Hello, World!" );
+    }
+
+    private static string getUsernameFromSecretsConfig() {
+        ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap
+        {
+            ExeConfigFilename = @"Config\Secrets.config"
+        };
+        
+        Configuration configuration = ConfigurationManager.OpenMappedExeConfiguration(
+            fileMap,
+            ConfigurationUserLevel.None
+        );
+
+        return configuration.AppSettings.Settings["username"].Value;
     }
 }
