a23b878ac297382eff1e6de3158c08859e7b4ace
themmixproject <steven.devalk@gmail.com>
2023-10-22 10:44:27 +0200

    Added loginUser() method, loaded in username and password from Secrets.config

--- SauceDemo/Program.cs
+++ SauceDemo/Program.cs
@@ -2,14 +2,18 @@
 using OpenQA.Selenium;
 using System;
 using System.Configuration;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
 
 internal class Program {
-    public static IWebDriver driver;
+    public static IWebDriver? driver;
     private static void Main( string[] args ) {
         string binaryPath = "C:\\Program Files (x86)\\Microsoft\\Edge\\Application\\msedge.exe";
         string driverPath = "C:\\Webdrivers\\msedgedriver.exe";
 
-        string username = getUsernameFromSecretsConfig();
+        Configuration secretsConfig = loadSecretsConfig();
+
+        string username = secretsConfig.AppSettings.Settings["username"].Value;
         string profilePath = "C:\\Users\\" + username +"\\AppData\\Local\\Microsoft\\Edge\\Test User Data";
 
         EdgeOptions options = new EdgeOptions {
@@ -25,12 +29,16 @@ internal class Program {
 
         driver = new EdgeDriver( driverPath, options );
 
+        string siteUsername = secretsConfig.AppSettings.Settings["site_username"].Value;
+        string sitePassword = secretsConfig.AppSettings.Settings["site_password"].Value;
+        loginUser( siteUsername, sitePassword );
+
         driver.Quit();
 
         Console.WriteLine( "Hello, World!" );
     }
 
-    private static string getUsernameFromSecretsConfig() {
+    private static Configuration loadSecretsConfig() {
         ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap
         {
             ExeConfigFilename = @"Config\Secrets.config"
@@ -41,6 +49,32 @@ internal class Program {
             ConfigurationUserLevel.None
         );
 
-        return configuration.AppSettings.Settings["username"].Value;
+        return configuration;
+    }
+
+    public static void loginUser(string username, string password ) {
+        Uri url = new Uri( "https://www.saucedemo.com/" );
+
+        driver!.Navigate().GoToUrl( url );
+
+        WebDriverWait loginWait = new WebDriverWait( driver, TimeSpan.FromSeconds( 5 ) );
+
+        loginWait.Until( ExpectedConditions.ElementIsVisible( By.Id( "user-name" ) ) );
+
+        IWebElement usernameField = driver.FindElement( By.Id( "user-name" ) );
+        usernameField.SendKeys( username );
+
+        IWebElement passwordField = driver.FindElement( By.Id( "password" ) );
+        passwordField.SendKeys( password );
+
+        IWebElement loginButton = driver.FindElement( By.Id( "login-button" ) );
+        loginButton.Click();
+
+        WebDriverWait inventoryHeaderWait = new WebDriverWait( driver, TimeSpan.FromSeconds( 2 ) );
+        inventoryHeaderWait.Until(
+            ExpectedConditions.ElementExists(
+                By.XPath( "//*[@id='header_container']/div[1]/div[2]/div" )
+            )
+        );
     }
 }
