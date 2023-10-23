53f35745749413e760d6aa82794f4e329d7cf075
themmixproject <steven.devalk@gmail.com>
2023-10-23 18:09:33 +0200

    Added SaveInventoryItemImage() and DownloadFileToDirectory() and CreateDownloadsFolder() methods, Fixed naming rule violations, added try/catch to quit driver when exception occurs, added Downloads folder to store downloaded files with selenium

--- SauceDemo/Program.cs
+++ SauceDemo/Program.cs
@@ -6,21 +6,31 @@ using OpenQA.Selenium.Support.UI;
 using SeleniumExtras.WaitHelpers;
 using SauceDemo.Factories;
 using System.Collections.ObjectModel;
+using System.Net;
+using System.Security.Policy;
+using OpenQA.Selenium.DevTools.V116.Emulation;
 
 internal class Program {
     public static IWebDriver driver;
     private static void Main( string[] args ) {
-        string driverPath = "C:\\Webdrivers\\msedgedriver.exe";
-        AppSettingsSection secrets = loadSecretsConfig();
-        string username = secrets.Settings["username"].Value;
-        string profilePath = "C:\\Users\\" + username +"\\AppData\\Local\\Microsoft\\Edge\\Test User Data";
-        driver = WebDriverFactory.GetEdgeDriver( profilePath, driverPath );
+        try {
+            string driverPath = "C:\\Webdrivers\\msedgedriver.exe";
+            AppSettingsSection secrets = loadSecretsConfig();
+            string username = secrets.Settings["username"].Value;
+            string profilePath = "C:\\Users\\" + username + "\\AppData\\Local\\Microsoft\\Edge\\Test User Data";
+            driver = WebDriverFactory.GetEdgeDriver( profilePath, driverPath );
 
-        string siteUsername = secrets.Settings["site_username"].Value;
-        string sitePassword = secrets.Settings["site_password"].Value;
-        loginUser( siteUsername, sitePassword );
+            string siteUsername = secrets.Settings["site_username"].Value;
+            string sitePassword = secrets.Settings["site_password"].Value;
+            LoginUser( siteUsername, sitePassword );
+            
+            loopThroughProducts();
+        }
+        catch ( Exception ex ) {
+            driver.Quit();
 
-        loopThroughProducts();
+            throw new Exception(ex.Message );
+        }
 
         driver.Quit();
 
@@ -28,13 +38,10 @@ internal class Program {
     }
 
     private static AppSettingsSection loadSecretsConfig() {
-        ExeConfigurationFileMap fileMap = new ExeConfigurationFileMap
-        {
-            ExeConfigFilename = @"Config\Secrets.config"
-        };
-        
         Configuration configuration = ConfigurationManager.OpenMappedExeConfiguration(
-            fileMap,
+            new ExeConfigurationFileMap {
+                ExeConfigFilename = @"Config\Secrets.config"
+            },
             ConfigurationUserLevel.None
         );
 
@@ -42,13 +49,13 @@ internal class Program {
     }
 
     public static void loopThroughProducts() {
-        ReadOnlyCollection<IWebElement> inventoryItems = getInventoryItems();
+        ReadOnlyCollection<IWebElement> inventoryItems = GetInventoryItems();
 
         for ( int i = 0; i < inventoryItems.Count; i++) {
             IWebElement inventoryItem = inventoryItems[i];
 
             if (elementIsStale( inventoryItem ) ) {
-                inventoryItems = getInventoryItems();
+                inventoryItems = GetInventoryItems();
                 inventoryItem = inventoryItems[i];
             }
 
@@ -57,17 +64,64 @@ internal class Program {
                 .FindElement( By.TagName( "a" ) );
             inventoryItemLink.Click();
 
+            SaveInventoryItemImage();
+
             driver.Navigate().Back();
         }
     }
 
-    public static ReadOnlyCollection<IWebElement> getInventoryItems() {
+    public static ReadOnlyCollection<IWebElement> GetInventoryItems() {
         return driver
             .FindElement( By.Id( "inventory_container" ) )
             .FindElements( By.CssSelector( ".inventory_item" ) );
     }
 
-    public static void loginUser(string username, string password ) {
+    public static void SaveInventoryItemImage() {
+        IWebElement inventoryImage = driver
+            .FindElement( By.Id( "inventory_item_container" ) )
+            .FindElement( By.ClassName( "inventory_details_img" ) );
+
+        string imageSource = inventoryImage.GetAttribute( "src" );
+        string imageName = Path.GetFileName(imageSource );
+
+        CreateDownloadsFolder();
+
+        string savePath = GetProjectDirectoryPath() + "\\Downloads";
+        DownloadFileToDirectory( imageSource, savePath, imageName );
+    }
+
+    public static string GetProjectDirectoryPath() {
+        return Directory.GetParent(
+            Directory.GetCurrentDirectory()!
+        )!.Parent!.Parent!.FullName;
+    }
+
+    public static async void DownloadFileToDirectory(
+        string imageSource,
+        string path,
+        string fileName
+    ) {
+        using (var client = new HttpClient()) {
+            var response = await client.GetAsync( imageSource );
+
+            using (var memoryStream = await response.Content.ReadAsStreamAsync()) {
+                string savePath = Path.Combine(path, fileName);
+                using (var fileStream = File.Create( savePath )) {
+                    memoryStream.CopyTo( fileStream );
+                }
+            }
+        }
+    }
+
+    public static void CreateDownloadsFolder() {
+        string path = GetProjectDirectoryPath() + "\\Downloads";
+
+        if (!Directory.Exists( path )) {
+            Directory.CreateDirectory( path );
+        }
+    }
+
+    public static void LoginUser(string username, string password ) {
         Uri url = new Uri( "https://www.saucedemo.com/" );
 
         driver!.Navigate().GoToUrl( url );
