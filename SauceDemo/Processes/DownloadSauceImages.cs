4df9ac1ba15c6bfd260283ef11251471fd0caa18
themmixproject <steven.devalk@gmail.com>
2023-10-28 15:27:10 +0200

    Added process code to DownloadSauceImage.cs

--- SauceDemo/Processes/DownloadSauceImages.cs
+++ SauceDemo/Processes/DownloadSauceImages.cs
@@ -1,9 +1,11 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.DevTools.V116.Target;
 using OpenQA.Selenium.Support.UI;
 using SauceDemo.Interfaces;
 using SeleniumExtras.WaitHelpers;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Configuration;
 using System.Linq;
 using System.Text;
@@ -22,10 +24,10 @@ namespace SauceDemo.Processes
         public void Run()
         {
             Uri url = new Uri( "https://www.saucedemo.com/" );
-
             Driver.Navigate().GoToUrl( url );
 
             LoginUser();
+            LoopThroughProducts();
 
             Driver.Quit();
         }
@@ -56,6 +58,79 @@ namespace SauceDemo.Processes
             );
         }
 
+        private void LoopThroughProducts()
+        {
+            ReadOnlyCollection<IWebElement> inventoryItems = GetInventoryItems();
+            for (int i = 0; i <inventoryItems.Count; i++)
+            {
+                IWebElement inventoryItem = inventoryItems[i];
+
+                if (ElementIsStale( inventoryItem ))
+                {
+                    inventoryItems = GetInventoryItems();
+                    inventoryItem = inventoryItems[i];
+                }
+
+                IWebElement inventoryItemLink = inventoryItem
+                    .FindElement( By.ClassName( "inventory_item_label" ) )
+                    .FindElement( By.TagName( "a" ) );
+                inventoryItemLink.Click();
+
+                SaveInventoryItemImage();
+
+                Driver.Navigate().Back();
+            }
+        }
+
+        private void SaveInventoryItemImage()
+        {
+            IWebElement inventoryImage = Driver
+                .FindElement( By.Id( "inventory_item_container" ) )
+                .FindElement( By.ClassName( "inventory_details_img" ) );
+
+            string imageSource = inventoryImage.GetAttribute( "src" );
+            string imageName = Path.GetFileName( imageSource );
+
+            CreateDownloadsFolder();
+
+            string savePath = GetProjectDirectoryPath() + "\\Downloads";
+            DownloadFileToDirectory( imageSource, savePath, imageName );
+        }
+
+        private void CreateDownloadsFolder()
+        {
+            string path = GetProjectDirectoryPath() + "\\Downloads";
+
+            if(!Directory.Exists( path ))
+            {
+                Directory.CreateDirectory( path );
+            }
+        }
+
+        private string GetProjectDirectoryPath()
+        {
+            return Directory.GetParent(
+                Directory.GetCurrentDirectory()!
+            )!.Parent!.Parent!.FullName;
+        }
+
+        private async void DownloadFileToDirectory(string fileUrl,  string path, string fileName )
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage response = await client.GetAsync( fileUrl );
+
+                using (Stream memoryStream = await response.Content.ReadAsStreamAsync())
+                {
+                    string savePath = Path.Combine( path, fileName );
+                    using (FileStream fileStream  = File.Create(savePath))
+                    {
+                        memoryStream.CopyTo( fileStream );
+                    }
+                }
+            }
+        }
+
         private static AppSettingsSection loadSecretsConfig()
         {
             Configuration configuration = ConfigurationManager.OpenMappedExeConfiguration(
@@ -67,6 +142,26 @@ namespace SauceDemo.Processes
             );
 
             return configuration.AppSettings;
+        }
+
+        private ReadOnlyCollection<IWebElement> GetInventoryItems()
+        {
+            return Driver
+                .FindElement( By.Id( "inventory_container" ) )
+                .FindElements( By.CssSelector( ".inventory_item" ) );
+        }
+
+        private bool ElementIsStale(IWebElement element )
+        {
+            try
+            {
+                bool _ = element.Displayed;
+                return false;
+            }
+            catch (StaleElementReferenceException)
+            {
+                return true;
+            }
         }
     }
 }
