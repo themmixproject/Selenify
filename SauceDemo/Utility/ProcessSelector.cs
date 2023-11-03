a3973659c6ee59123599e123e10f041529d9156e
themmixproject <steven.devalk@gmail.com>
2023-11-03 23:57:44 +0100

    Added ProcessSelector class file

---
+++ SauceDemo/Utility/ProcessSelector.cs
@@ -0,0 +1,31 @@
+﻿using SauceDemo.Processes;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SauceDemo.Utility
+{
+    public static class ProcessSelector
+    {
+        public static List<IProcessBase> GetProcesses()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            var types = assembly.GetTypes()
+                .Where( t => typeof( IProcessBase ).IsAssignableFrom( t ) && !t.IsAbstract )
+            .ToList();
+
+            var processes = new List<IProcessBase>();
+            foreach (Type type in types)
+            {
+                var process = Activator.CreateInstance( type ) as IProcessBase;
+                processes.Add( process! );
+            }
+
+            return processes;
+        }
+    }
+}
