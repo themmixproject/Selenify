e3e9011f41b8e6fa5531b8893d8db9c9083d0648
themmixproject <steven.devalk@gmail.com>
2023-11-04 15:42:52 +0100

    Added SelectProcess() method in static ProcessSelector class

--- SauceDemo/Utility/ProcessSelector.cs
+++ SauceDemo/Utility/ProcessSelector.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -26,6 +27,74 @@ namespace SauceDemo.Utility
             }
 
             return processes;
+        }
+
+        public static IProcessBase? SelectProcess() {
+            List<IProcessBase> processes = GetProcesses();
+            int processesCount = processes.Count;
+            int currentOptionIndex = 0;
+            
+            int GetNextOptionIndex() {
+                int selectedIndex = currentOptionIndex;
+                selectedIndex += 1;
+
+                if (selectedIndex > processesCount - 1) {
+                    selectedIndex = 0;
+                }
+
+                return selectedIndex;
+            }
+
+            int GetPreviousOptionIndex() {
+                int selectedIndex = currentOptionIndex;
+                selectedIndex -= 1;
+
+                if (selectedIndex < 0) {
+                    selectedIndex = processesCount - 1;
+                }
+
+                return selectedIndex;
+            }
+
+            int countStringLength = processes.Count.ToString()
+                .Length;
+            bool userHasNotConfirmed = true;
+            while( userHasNotConfirmed ) {
+                string headerString = "Select a process <" + 
+                    (currentOptionIndex + 1)
+                        .ToString()
+                        .PadLeft(countStringLength) +
+                    "/" + 
+                    processesCount + "> :\n";
+                string processName = processes[currentOptionIndex].ProcessName;
+                string footer = "\n\nPress enter to continue.";
+                string uiString = headerString + processName + footer;
+                ConsoleUI.Write( uiString );
+
+                ConsoleKeyInfo key = Console.ReadKey();
+                ConsoleKey keyCode = key.Key;
+
+
+                if (keyCode == ConsoleKey.LeftArrow) {
+                    currentOptionIndex = GetPreviousOptionIndex();
+                }
+                else if (keyCode == ConsoleKey.RightArrow) {
+                    currentOptionIndex = GetNextOptionIndex();
+                }
+                else if (keyCode == ConsoleKey.Enter) {
+                    userHasNotConfirmed = false;
+                    ConsoleUI.Clear();
+                    ConsoleUI.Stop();
+                    return processes[currentOptionIndex];
+                }
+                else if (keyCode == ConsoleKey.Escape ) {
+                    ConsoleUI.Clear();
+                    ConsoleUI.Stop();
+                    userHasNotConfirmed = false;
+                }
+            }
+
+            return null;
         }
     }
 }
