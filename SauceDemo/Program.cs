2eb4c728357f91cfbb718acca622d273c4309fcf
themmixproject <steven.devalk@gmail.com>
2023-11-04 15:43:51 +0100

    Called SelectProcess in Program.cs

--- SauceDemo/Program.cs
+++ SauceDemo/Program.cs
@@ -10,23 +10,6 @@ using SauceDemo.Utility;
 
 internal class Program {
     private static void Main( string[] args ) {
-        //WebDriverManager.LoadEdgeDriver();
-
-        // ProcessSelector.GetProcesses();
-
-
-        //DownloadSauceImages process = new DownloadSauceImages();
-        //process.Run();
-        Console.WriteLine( "hello world!" );
-        ConsoleUI.Write( "Hello\nWorld!\nI am Steven!" );
-        System.Threading.Thread.Sleep( 2000 );
-        ConsoleUI.Write( "Banana,\nMango,\nApples" );
-        System.Threading.Thread.Sleep( 2000 );
-        ConsoleUI.Stop();
-        Console.WriteLine( "This line won't be cleared by ConsoleUI." );
-        System.Threading.Thread.Sleep( 2000 );
-        ConsoleUI.Write( "Test1" );
-        System.Threading.Thread.Sleep( 2000 );
-        ConsoleUI.Write( "Test2" );
+        IProcessBase selectedProcess = ProcessSelector.SelectProcess()!;
     }
 }
