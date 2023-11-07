62852b9325bf3fa988179d6222ed4dd3d99f3203
themmixproject <steven.devalk@gmail.com>
2023-11-07 18:21:53 +0100

    Re-enabled ProcessSelector.SelectProcess() call in Program.Main

--- Selenify/Program.cs
+++ Selenify/Program.cs
@@ -10,11 +10,8 @@ using Selenify.Utility;
 
 internal class Program {
     private static void Main( string[] args ) {
-        //IProcessBase selectedProcess = ProcessSelector.SelectProcess()!;
-        ConsoleUI.WriteLines("Line 1", "Line 2", "Line 3");
-        Thread.Sleep(2000);
-        ConsoleUI.UpdateLine(1, "Hello world!");
-        Thread.Sleep(2000);
-        var i = 0;
+        IProcessBase selectedProcess = ProcessSelector.SelectProcess()!;
+
+
     }
 }
