3a17ab03d36f8b3e5709df6d3beb702fea4b7f93
themmixproject <steven.devalk@gmail.com>
2023-11-07 18:21:14 +0100

    Added new basic process to test selection UI

---
+++ Selenify/Processes/AddProductToCart.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenify.Processes
+{
+    public class AddProductToCart : ProcessBase<AddProductToCart.ProcessState>
+    {
+        public class ProcessState { }
+        public AddProductToCart() : base("Add Product To Cart") { }
+        public override void Run () { }
+    }
+}
