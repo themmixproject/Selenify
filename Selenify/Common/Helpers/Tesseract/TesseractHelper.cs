2498af89bc518faf8ba9c627dac801e348eaa784
themmixproject <steven.devalk@gmail.com>
2023-11-25 15:38:50 +0100

    Added TesseractHelper class with GetTextFromImage method and added Tesseract english language data

---
+++ Selenify/Common/Helpers/Tesseract/TesseractHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tesseract;
+
+namespace Selenify.Common.Helpers.Tesseract
+{
+    public static class TesseractHelper
+    {
+        public static string GetTextFromImage(string imagePath)
+        {
+            using (TesseractEngine engine = new TesseractEngine(@"./tessdata", "eng", EngineMode.Default))
+            {
+                using (var image = Pix.LoadFromFile(imagePath))
+                {
+                    using (var page = engine.Process(image))
+                    {
+                        return page.GetText();
+                    }
+                }
+            }
+        }
+    }
+}
