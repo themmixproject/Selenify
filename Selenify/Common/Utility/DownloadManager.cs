807d9e0a27e3d0bba6d3f4fcf26c72bd63c02d65
themmixproject <steven.devalk@gmail.com>
2023-11-26 23:28:06 +0100

    Added DownloadFileWithCookiesAndProgressBarAsync and DownloadFileWithCookiesAsync methods to DownloadManager, replaced ReadAsStreamAsync with ReadAsStream in DownloadFileWithProgressBarAsync method, made return value void in DownloadFileWithProgressBarAsync

--- Selenify/Common/Utility/DownloadManager.cs
+++ Selenify/Common/Utility/DownloadManager.cs
@@ -7,12 +7,37 @@ using System.Text;
 using System.Threading.Tasks;
 using Selenify.Common.Helpers;
 using System.IO;
+using static Selenify.Common.Utility.WebDriverManager;
+using System.Net;
 
 namespace Selenify.Common.Utility
 {
     public static class DownloadManager
     {
-        public static async Task DownloadFileWithProgressBarAsync(string fileUrl, string path, string progressPrefix = "")
+        public static async void DownloadFileWithCookiesAndProgressBarAsync(string fileUrl, string path, string progressPrefix)
+        {
+            ConsoleProgressBar progressBar = new ConsoleProgressBar();
+            var handler = new HttpClientHandler();
+            handler.CookieContainer = CreateCookieContainerFromWebDriverCookies();
+            using (HttpClient client = new HttpClient(handler))
+            {
+                client.Timeout = TimeSpan.FromMinutes(5);
+
+                string savePath;
+                using (Stream stream = client.GetAsync(fileUrl).Result.Content.ReadAsStream())
+                {
+                    savePath = GetFilePathForDownload(fileUrl, path, stream);
+                }
+
+                using (var file = new FileStream(savePath, FileMode.Create, FileAccess.Write, FileShare.None))
+                {
+                    await HttpClientExtensions.DownloadAsync(client, fileUrl, file, progress, new CancellationToken());
+                }
+            }
+
+        }
+
+        public static async void DownloadFileWithProgressBarAsync(string fileUrl, string path, string progressPrefix = "")
         {
             var progress = new ConsoleProgressBar(progressPrefix);
             using (var client = new HttpClient())
@@ -20,7 +45,7 @@ namespace Selenify.Common.Utility
                 client.Timeout = TimeSpan.FromMinutes(5);
 
                 string savePath;
-                using (var stream = HttpClientHelper.GetAsync(fileUrl).Result.Content.ReadAsStreamAsync().Result)
+                using (var stream = HttpClientHelper.GetAsync(fileUrl).Result.Content.ReadAsStream())
                 {
                     savePath = GetFilePathForDownload(fileUrl, path, stream);
                 }
@@ -47,6 +72,27 @@ namespace Selenify.Common.Utility
             }
         }
 
+        public static async void DownloadFileWithCookiesAsync(string fileUrl, string path)
+        {
+            var handler = new HttpClientHandler();
+            handler.CookieContainer = CreateCookieContainerFromWebDriverCookies();
+            using(HttpClient tempClient = new HttpClient(handler))
+            {
+                HttpResponseMessage response = await tempClient.GetAsync(fileUrl);
+                HttpContent responseContent = response.Content;
+                using (var stream = await responseContent.ReadAsStreamAsync())
+                {
+                    string savePath = GetFilePathForDownload(fileUrl, path, stream);
+                    using (FileStream fileStream = new FileStream(savePath, FileMode.Create, FileAccess.Write))
+                    {
+                        stream.Position = 0;
+                        await stream.CopyToAsync(fileStream);
+                    }
+                }
+            }
+        }
+
+
         private static string GetFilePathForDownload(string fileUrl, string path, Stream fileStream)
         {
             string urlWithoutQuery = new Uri(fileUrl).GetLeftPart(UriPartial.Path);
@@ -59,6 +105,21 @@ namespace Selenify.Common.Utility
             return Path.Combine(saveDirectory!, fileName);
         }
 
+        private static CookieContainer CreateCookieContainerFromWebDriverCookies()
+        {
+            var cookies = Driver.Manage().Cookies.AllCookies;
+            CookieContainer cookieContainer = new CookieContainer();
+            foreach (OpenQA.Selenium.Cookie cookie in cookies)
+            {
+                cookieContainer.Add(
+                    new System.Net.Cookie(
+                        cookie.Name,
+                        cookie.Value,
+                        cookie.Path,
+                        cookie.Domain));
+            }
 
+            return cookieContainer;
+        }
     }
 }
