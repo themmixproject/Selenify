1b76885932790717443af1cc969496a93bcc7c93
themmixproject <steven.devalk@gmail.com>
2023-11-26 23:00:40 +0100

    Processed renaming of Downloadfile method in Program.cs

--- Selenify/Program.cs
+++ Selenify/Program.cs
@@ -23,6 +23,6 @@ internal class Program {
         string downloadsDir = Directory.GetParent(Directory.GetCurrentDirectory())!.Parent!.Parent!.FullName + "\\Downloads\\";
         FileHelper.CreateDirectoryIfNotExists( downloadsDir );
         //DownloadManager.DownloadFile("https://lh4.googleusercontent.com/-MC8WtWcan0I/AAAAAAAAAAI/AAAAAAAAAQI/bSW8SUtaZDU/photo.jpg?sz=192", downloadsDir);
-        DownloadManager.DownloadFile("https://th.bing.com/th/id/OIP.-Sf7ke25iuyCKqmNG664GwHaFJ?rs=1&pid=ImgDetMain", downloadsDir);
+        DownloadManager.DownloadFileAsync("https://th.bing.com/th/id/OIP.-Sf7ke25iuyCKqmNG664GwHaFJ?rs=1&pid=ImgDetMain", downloadsDir);
     }
 }
