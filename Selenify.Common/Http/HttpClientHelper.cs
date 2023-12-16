ee31c9bec87d22c34bcba63d95c67a2fcc0a0725
themmixproject <steven.devalk@gamil.com>
2023-12-16 18:27:40 +0100

    Added HttpClientHelper class containing CreateClientWithCookieContainer method to directly create a client with a cookiecontainer

---
+++ Selenify.Common/Http/HttpClientHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Selenify.Common.Http
+{
+    static class HttpClientHelper
+    {
+        public static HttpClient CreateClientWithCookieContainer(CookieContainer cookieContainer)
+        {
+            HttpClientHandler handler = new HttpClientHandler{
+                CookieContainer = cookieContainer
+            };
+
+            return new HttpClient(handler);
+        }
+    }
+}
